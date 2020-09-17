8737a03969011f6d0c6c10d53657c2280ef43bbd
nobuko.m <animopara100pre@gmail.com>
2020-09-17 11:57:58 +0900

    chaprer2_ 練習問題

---
+++ chapter2.rb
@@ -0,0 +1,23 @@
+# (1),(2)
+puts 2 + 3
+puts 2 ** 2 * 3.14
+
+#(3),(4),(5)
+puts "Ruby"
+puts "abc" + "def"
+puts "123".to_i + "456".to_i
+
+#(6),(7)
+coffee = 300
+espresso = 100
+puts "コーヒー：#{coffee}円"
+puts "合計：#{coffee + espresso * 2}"
+coffee = 400
+puts "合計：#{coffee + espresso * 2}"
+
+#(8)
+total = 300 * 2
+tax = total * 0.08
+p total + tax
+
+#(9)
